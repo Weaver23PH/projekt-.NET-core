a1c0d09c854e8377c2a21a5dec0c71af8a55decd
Weaver23PH <themichalcarlo@gmail.com>
2020-08-19 21:21:27 +0200

    Added admin user table

---
+++ WebApplication3/Controllers/AdminController.cs
@@ -0,0 +1,262 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Identity.Models;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System;
+using Identity.Email;
+
+namespace Identity.Controllers
+{
+    public class AdminController : Controller
+    {
+        private UserManager<IdentityUser> userManager;
+        private IPasswordHasher<IdentityUser> passwordHasher;
+        private IPasswordValidator<IdentityUser> passwordValidator;
+        private IUserValidator<IdentityUser> userValidator;
+
+        /*public AdminController(UserManager<IdentityUser> usrMgr, IPasswordHasher<IdentityUser> passwordHash)
+        {
+            userManager = usrMgr;
+            passwordHasher = passwordHash;
+        }*/
+
+        public AdminController(UserManager<IdentityUser> usrMgr, IPasswordHasher<IdentityUser> passwordHash, IPasswordValidator<IdentityUser> passwordVal, IUserValidator<IdentityUser> userValid)
+        {
+            userManager = usrMgr;
+            passwordHasher = passwordHash;
+            passwordValidator = passwordVal;
+            userValidator = userValid;
+        }
+
+        public IActionResult Index()
+        {
+            return View(userManager.Users);
+        }
+
+        public ViewResult Create() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Create(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityUser IdentityUser = new IdentityUser
+                {
+                    UserName = user.Name,
+                    Email = user.Email
+                };
+
+                IdentityResult result = await userManager.CreateAsync(IdentityUser, user.Password);
+                if (result.Succeeded)
+                {
+                    var token = await userManager.GenerateEmailConfirmationTokenAsync(IdentityUser);
+                    var confirmationLink = Url.Action("ConfirmEmail", "Email", new { token, email = user.Email }, Request.Scheme);
+                    EmailHelper emailHelper = new EmailHelper();
+                    bool emailResponse = emailHelper.SendEmail(user.Email, confirmationLink);
+
+                    if (emailResponse)
+                        return RedirectToAction("Index");
+                    else
+                    {
+                        // log email failed 
+                    }
+                }
+                else
+                {
+                    foreach (IdentityError error in result.Errors)
+                        ModelState.AddModelError("", error.Description);
+                }
+            }
+            return View(user);
+        }
+
+        /*[HttpPost]
+        public async Task<IActionResult> Create(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityUser IdentityUser = new IdentityUser
+                {
+                    UserName = user.Name,
+                    Email = user.Email,
+                    Country = user.Country,
+                    Age = user.Age,
+                    Salary = user.Salary
+                };
+
+                IdentityResult result = await userManager.CreateAsync(IdentityUser, user.Password);
+                if (result.Succeeded)
+                    return RedirectToAction("Index");
+                else
+                {
+                    foreach (IdentityError error in result.Errors)
+                        ModelState.AddModelError("", error.Description);
+                }
+            }
+            return View(user);
+        }*/
+
+        public async Task<IActionResult> Update(string id)
+        {
+            IdentityUser user = await userManager.FindByIdAsync(id);
+            if (user != null)
+                return View(user);
+            else
+                return RedirectToAction("Index");
+        }
+
+        /*[HttpPost]
+        public async Task<IActionResult> Update(string id, string email, string password)
+        {
+            IdentityUser user = await userManager.FindByIdAsync(id);
+            if (user != null)
+            {
+                if (!string.IsNullOrEmpty(email))
+                    user.Email = email;
+                else
+                    ModelState.AddModelError("", "Email cannot be empty");
+
+                if (!string.IsNullOrEmpty(password))
+                    user.PasswordHash = passwordHasher.HashPassword(user, password);
+                else
+                    ModelState.AddModelError("", "Password cannot be empty");
+
+                if (!string.IsNullOrEmpty(email) && !string.IsNullOrEmpty(password))
+                {
+                    IdentityResult result = await userManager.UpdateAsync(user);
+                    if (result.Succeeded)
+                        return RedirectToAction("Index");
+                    else
+                        Errors(result);
+                }
+            }
+            else
+                ModelState.AddModelError("", "User Not Found");
+            return View(user);
+        }*/
+
+        [HttpPost]
+        public async Task<IActionResult> Update(string id, string email, string password)
+        {
+            IdentityUser user = await userManager.FindByIdAsync(id);
+            if (user != null)
+            {
+                IdentityResult validEmail = null;
+                if (!string.IsNullOrEmpty(email))
+                {
+                    validEmail = await userValidator.ValidateAsync(userManager, user);
+                    if (validEmail.Succeeded)
+                        user.Email = email;
+                    else
+                        Errors(validEmail);
+                }
+                else
+                    ModelState.AddModelError("", "Email cannot be empty");
+
+                IdentityResult validPass = null;
+                if (!string.IsNullOrEmpty(password))
+                {
+                    validPass = await passwordValidator.ValidateAsync(userManager, user, password);
+                    if (validPass.Succeeded)
+                        user.PasswordHash = passwordHasher.HashPassword(user, password);
+                    else
+                        Errors(validPass);
+                }
+                else
+                    ModelState.AddModelError("", "Password cannot be empty");
+
+                if (validEmail != null && validPass != null && validEmail.Succeeded && validPass.Succeeded)
+                {
+                    IdentityResult result = await userManager.UpdateAsync(user);
+                    if (result.Succeeded)
+                        return RedirectToAction("Index");
+                    else
+                        Errors(result);
+                }
+            }
+            else
+                ModelState.AddModelError("", "User Not Found");
+
+            return View(user);
+        }
+
+        /*[HttpPost]
+        public async Task<IActionResult> Update(string id, string email, string password, int age, string country, string salary)
+        {
+            IdentityUser user = await userManager.FindByIdAsync(id);
+            if (user != null)
+            {
+                IdentityResult validEmail = null;
+                if (!string.IsNullOrEmpty(email))
+                {
+                    validEmail = await userValidator.ValidateAsync(userManager, user);
+                    if (validEmail.Succeeded)
+                        user.Email = email;
+                    else
+                        Errors(validEmail);
+                }
+                else
+                    ModelState.AddModelError("", "Email cannot be empty");
+
+                IdentityResult validPass = null;
+                if (!string.IsNullOrEmpty(password))
+                {
+                    validPass = await passwordValidator.ValidateAsync(userManager, user, password);
+                    if (validPass.Succeeded)
+                        user.PasswordHash = passwordHasher.HashPassword(user, password);
+                    else
+                        Errors(validPass);
+                }
+                else
+                    ModelState.AddModelError("", "Password cannot be empty");
+
+                user.Age = age;
+
+                Country myCountry;
+                Enum.TryParse(country, out myCountry);
+                user.Country = myCountry;
+
+                if (!string.IsNullOrEmpty(salary))
+                    user.Salary = salary;
+                else
+                    ModelState.AddModelError("", "Salary cannot be empty");
+
+                if (validEmail != null && validPass != null && validEmail.Succeeded && validPass.Succeeded && !string.IsNullOrEmpty(salary))
+                {
+                    IdentityResult result = await userManager.UpdateAsync(user);
+                    if (result.Succeeded)
+                        return RedirectToAction("Index");
+                    else
+                        Errors(result);
+                }
+            }
+            else
+                ModelState.AddModelError("", "User Not Found");
+
+            return View(user);
+        }*/
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(string id)
+        {
+            IdentityUser user = await userManager.FindByIdAsync(id);
+            if (user != null)
+            {
+                IdentityResult result = await userManager.DeleteAsync(user);
+                if (result.Succeeded)
+                    return RedirectToAction("Index");
+                else
+                    Errors(result);
+            }
+            else
+                ModelState.AddModelError("", "User Not Found");
+            return View("Index", userManager.Users);
+        }
+
+        private void Errors(IdentityResult result)
+        {
+            foreach (IdentityError error in result.Errors)
+                ModelState.AddModelError("", error.Description);
+        }
+    }
+}
